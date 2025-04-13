92d0aedcb3272f723ddb0224860c895bbe16398f
shailesh097 <sailesh.pokharel.234@gmail.com>
2025-04-14 00:47:04 +0200

    adding different paths

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a specific snippet."))
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Println("Starting server on: http://localhost:4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
